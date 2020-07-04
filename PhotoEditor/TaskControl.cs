056a3cd1b4366b34e7aaa5fb777bbfaf75ab7016
Martin Stevko <mstevko10@gmail.com>
2020-07-04 16:38:26 +0200

    Image tasks controlling mechanism

--- PhotoEditor/TaskControl.cs
+++ PhotoEditor/TaskControl.cs
@@ -1,13 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace PhotoEditor
 {
     class TaskControl
     {
+        private MainForm form;
 
+        private ImageTask head;
+        private ImageTask tail;
+
+        private ImageTask headWaiting;
+        private ImageTask tailWaiting;
+
+        public TaskControl(MainForm form)
+        {
+            this.form = form;
+
+            head = null;
+            tail = null;
+
+            headWaiting = null;
+            tailWaiting = null;
+
+            Thread t = new Thread(Process);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        public void Add(ImageTask task)
+        {
+            if ((head == null) || (!head.ongoing))
+            {
+                if (head == null)
+                {
+                    head = task;
+                    tail = task;
+                }
+                else
+                {
+                    if (tail.taskType == task.taskType)
+                    {
+                        if (tail.previous == null)
+                        {
+                            head = task;
+                            tail = task;
+                        }
+                        else
+                        {
+                            tail.previous.next = task;
+                            task.previous = tail.previous;
+                            tail = task;
+                        }
+                    }
+                    else
+                    {
+                        tail.next = task;
+                        task.previous = tail;
+                        tail = task;
+                    }
+                }
+            }
+            else
+            {
+                if (headWaiting == null)
+                {
+                    headWaiting = task;
+                    tailWaiting = task;
+                }
+                else
+                {
+                    if (tailWaiting.taskType == task.taskType)
+                    {
+                        if (tailWaiting.previous == null)
+                        {
+                            headWaiting = task;
+                            tailWaiting = task;
+                        }
+                        else
+                        {
+                            tailWaiting.previous.next = task;
+                            task.previous = tailWaiting.previous;
+                            tailWaiting = task;
+                        }
+                    }
+                    else
+                    {
+                        tailWaiting.next = task;
+                        task.previous = tailWaiting;
+                        tailWaiting = task;
+                    }
+                }
+            }
+        }
+
+        private void Process()
+        {
+            if (head != null)
+            {
+                head.ongoing = true;
+                head.Process(form.imageSet);
+                head = head.next;
+                if (head != null)
+                {
+                    head.previous = null;
+                }
+                form.RedrawImageSet();
+            }
+            else
+            {
+                Thread.Sleep(100);
+            }
+            Process();
+        }
+
+        public void CheckAndProcess()
+        {
+            if (head == null)
+            {
+                head = headWaiting;
+                tail = tailWaiting;
+                headWaiting = null;
+                tailWaiting = null;
+            }
+            else if (head.ongoing)
+            {
+                Thread.Sleep(100);
+                CheckAndProcess();
+            }
+            else
+            {
+                if (headWaiting != null)
+                {
+                    tail.next = headWaiting;
+                    headWaiting.previous = tail;
+                    tail = tailWaiting;
+                }
+            }
+        }
     }
 }
