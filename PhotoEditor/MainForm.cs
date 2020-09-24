26b8f522c39352ce5fbb46e24c1605e5d2dae372
Martin Stevko <mstevko10@gmail.com>
2020-09-24 17:04:57 +0200

    Image load control draft

--- PhotoEditor/MainForm.cs
+++ PhotoEditor/MainForm.cs
@@ -192,46 +192,6 @@ namespace PhotoEditor
 
         #endregion
 
-        #region Image load control
-
-        /// <summary>
-        /// Opens dialog window to choose image and 
-        /// prepares main form for image load
-        /// </summary>
-        private void OpenNewImage()
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "All Images|*.jpg;*.bmp;*.png";
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                ResetSettings();
-                string filename = dialog.FileName;
-                Thread thr = new Thread(LoadNewImage);
-                thr.Start(filename);
-            }
-            log.Add("Image opened");
-        }
-
-        /// <summary>
-        /// Loads new image, prepares thumnails and 
-        /// shows everything in the main form
-        /// </summary>
-        /// <param name="data">Image file name (from parallel thread runner)</param>
-        private void LoadNewImage(object data)
-        {
-            string filename = data.ToString();
-            imageSet.Load(
-                filename,
-                pictureBox2.Width,
-                pictureBox2.Height,
-                button3.Width - 26,
-                button3.Height - 10
-            );
-            RedrawImageSet();
-        }
-
-        #endregion
-
         #region Image setting controls
 
         #region Control values synchronization
