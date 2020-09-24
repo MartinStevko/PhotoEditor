1c42284679a98527c47d6013285f5477e2385710
Martin Stevko <mstevko10@gmail.com>
2020-09-24 16:45:18 +0200

    Help menu events

--- PhotoEditor/MainForm.cs
+++ PhotoEditor/MainForm.cs
@@ -431,53 +431,6 @@ namespace PhotoEditor
 
         #endregion
 
-        #region Help menu events
-
-        /// <summary>
-        /// Show app menu under 'Help' - User manual, Documentation, Report issue
-        /// </summary>
-        private void button9_Click(object sender, EventArgs e)
-        {
-            panel3.Visible = false;
-            panel4.Visible = false;
-            panel5.Visible = !panel5.Visible;
-        }
-
-        /// <summary>
-        /// Help -> User manual menu button - opens user manual in user default web browser
-        /// </summary>
-        private void button21_Click(object sender, EventArgs e)
-        {
-            panel5.Visible = false;
-            string targetURL = @"https://github.com/MartinStevko/PhotoEditor/wiki/User-manual";
-            System.Diagnostics.Process.Start(targetURL);
-            log.Add("User manual opened");
-        }
-
-        /// <summary>
-        /// Help -> Documentation menu button - opens program documentation in user default web browser
-        /// </summary>
-        private void button20_Click(object sender, EventArgs e)
-        {
-            panel5.Visible = false;
-            string targetURL = @"https://github.com/MartinStevko/PhotoEditor/wiki/Documentation";
-            System.Diagnostics.Process.Start(targetURL);
-            log.Add("Program documentation opened");
-        }
-
-        /// <summary>
-        /// Help -> Report issue menu button - opens report issue website in user default web browser
-        /// </summary>
-        private void button19_Click(object sender, EventArgs e)
-        {
-            panel5.Visible = false;
-            string targetURL = @"https://github.com/MartinStevko/PhotoEditor/issues/new/choose";
-            System.Diagnostics.Process.Start(targetURL);
-            log.Add("Report issue form opened");
-        }
-
-        #endregion
-
         #region Image load control
 
         /// <summary>
